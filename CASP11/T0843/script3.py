816ef04ce7f7e2fbdab0ff0237db6d30931fb9f9
SaiManikanta <infomanikanta@gmail.com>
2016-11-18 14:22:13 -0600

    T0843 Update trying with different protein sequence 4zwvA

--- CASP11/T0843/script3.py
+++
@@ -1,9 +0,0 @@
-from modeller import *
-env = environ()
-aln = alignment(env)
-mdl = model(env, file='4xauA', model_segment=('FIRST:A','LAST:A'))
-aln.append_model(mdl, align_codes='4xauA', atom_files='4xauA.pdb')
-aln.append(file='qseq1.ali', align_codes='qseq1')
-aln.align2d()
-aln.write(file='qseq1-4xauA.ali', alignment_format='PIR')
-aln.write(file='qseq1-4xauA.pap', alignment_format='PAP')
